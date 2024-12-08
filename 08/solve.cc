464594b8f72300e70c3535e18fa765480f2c1b6e
Peter Edwards <peadar@arista.com>
2024-12-08 15:53:55 +0000

    Day 8

---
+++ 08/solve.cc
@@ -0,0 +1,93 @@
+#include "aoc.h"
+
+template <typename Scalar> struct Point {
+   Scalar row{};
+   Scalar col{};
+   Point operator + (const Point &rhs) const noexcept { return { row + rhs.row, col + rhs.col }; }
+   Point &operator += (const Point &rhs) noexcept {
+      row += rhs.row;
+      col += rhs.col;
+      return *this;
+   }
+   Point operator - (const Point &rhs) const noexcept { return { row - rhs.row, col - rhs.col }; }
+   Point operator - () const noexcept { return { -row, -col }; }
+   Point operator * (Scalar m) const noexcept { return { row * m, col * m }; }
+   Point abs () const { return { std::abs(row), std::abs(col) }; }
+   auto operator <=> (const Point &rhs) const noexcept = default;
+   bool operator == (const Point &rhs) const noexcept = default;
+   unsigned magnitude() const noexcept { return std::abs(row) + std::abs(col); }
+};
+
+namespace std {
+template <typename T> struct hash<Point<T>> {
+   size_t operator() (const Point<T> &p) const noexcept { return p.row * 50 + p.col; }
+};
+}
+
+using Int = int;
+using P = Point<Int>;
+using PointSet = std::unordered_set<P>;
+
+struct Input {
+   std::unordered_map<char, PointSet> points;
+   P size;
+   Input(std::istream &is) {
+      Int row{}, col{};
+      for (std::string line; getline(is, line);) {
+         col = 0;
+         for (char c : line) {
+            if (c != '.')
+               points[c].insert({row, col});
+            ++col;
+         }
+         ++row;
+      }
+      size = { row, col };
+   }
+};
+
+struct Part1 : Input {
+   Part1(std::istream &is) : Input(is) { }
+   Int solve() const noexcept {
+      PointSet antinodes;
+      for (auto &[c, cpoints] : points) {
+         if (c == '#')
+            continue;
+         for (const P &a : cpoints) {
+            for (const P &b : cpoints) {
+               if (a == b)
+                  continue;
+               auto anti = b + (b - a);
+               if (anti.row >= 0 && anti.col >= 0 && anti.row < size.row && anti.col < size.col)
+                  antinodes.insert(anti);
+            }
+         }
+      }
+      return antinodes.size();
+   }
+};
+
+struct Part2 : Input {
+   Part2(std::istream &is) : Input(is) {}
+   Int solve() const noexcept {
+      PointSet antinodes;
+      for (auto &[c, cpoints] : points) {
+         if (c == '#')
+            continue;
+         for (auto ia = cpoints.begin(); ia != cpoints.end() ; ++ia) {
+            for (auto ib = cpoints.begin(); ib != cpoints.end() ; ++ib) {
+               if (ia == ib)
+                  continue;
+               auto diff = *ib - *ia;
+               for (auto p = *ia; p.row < size.row && p.col < size.col && p.row >= 0 && p.col >= 0; p += diff)
+                  antinodes.insert(p);
+            }
+         }
+      }
+      return antinodes.size();
+   }
+};
+
+aoc::Case part1("part1", [](std::istream &is, std::ostream &os) {os << Part1{is}.solve();});
+aoc::Case part2("part2", [](std::istream &is, std::ostream &os) {os << Part2{is}.solve();});
+aoc::Case parse("parse", [](std::istream &is, std::ostream &) {Input{is};});
