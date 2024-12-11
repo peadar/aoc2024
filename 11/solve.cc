0664fd45cdb686c2ff303589715788a3072b40f7
Peter Edwards <peadar@arista.com>
2024-12-11 13:36:21 +0000

    faster?

--- 11/solve.cc
+++ 11/solve.cc
@@ -5,28 +5,42 @@ using Int = unsigned long;
 using Ints = std::vector<Int>;
 using IntMap = std::unordered_map<Int, Int>;
 
-struct State {
+Ints parse(std::istream &in) {
    Ints sequence;
-   State(std::istream &is) {
-      for (Int i;;) {
-         is >> i;
-         if (!is)
-            break;
-         sequence.push_back(i);
-      }
+   for (Int i;;) {
+      in >> i;
+      if (!in)
+         break;
+      sequence.push_back(i);
    }
+   return sequence;
 };
 
-constexpr unsigned digit_count(Int i) {
+constexpr inline unsigned digit_count(Int i) noexcept {
    unsigned result = 1;
    for (Int compare = 10;; compare *=10, result++)
       if (compare > i)
          return result;
 }
 
+constexpr Int pow10(unsigned count) noexcept {
+   switch (count) { // this measures margiinally faster than a loop multiplying
+                    // by 10 each time. Additional cases beyond 4 don't help.
+      case 0: return 1;
+      case 1: return 10;
+      case 2: return 100;
+      case 3: return 1000;
+      case 4: return 10000;
+      default: {
+         Int half = count / 2;
+         return pow10(half) * pow10(count - half);
+      }
+   }
+}
+
 void permute_one( Int i, auto yield ) {
    if (i == 0) {
-     yield(1);
+      yield(1);
    } else {
       unsigned digits = digit_count(i);
       if (digits % 2 == 0) {
@@ -40,12 +54,9 @@ void permute_one( Int i, auto yield ) {
 }
 
 Int permute(Ints &&seq, size_t iters) {
-   IntMap a, b;
-   IntMap *in, *out;
+   IntMap a, *in=&a, b, *out=&b;
    for (auto i: seq)
-      a[i]++;
-   in = &a;
-   out = &b;
+      (*in)[i]++;
    for (size_t iter = 0; iter < iters; ++iter) {
       for (auto &[ink, incount] : *in)
          permute_one( ink, [&] (Int outk) { (*out)[outk] += incount; });
@@ -58,9 +69,9 @@ Int permute(Ints &&seq, size_t iters) {
 }
 
 aoc::Case part1{ "part1", [](std::istream &is, std::ostream &os) {
-   os << permute( State{is}.sequence, 25 );
+   os << permute(parse(is), 25 );
 }};
 
 aoc::Case part2{ "part2", [](std::istream &is, std::ostream &os) {
-   os << permute( State{is}.sequence, 75 );
+   os << permute( parse(is), 75 );
 }};
