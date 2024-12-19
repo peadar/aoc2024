f7a5fb5ecf577de6837ca9bac5f29d7a186d1101
Peter Edwards <peadar@arista.com>
2024-12-19 21:20:52 +0000

    golf a bit

--- 19/solve.cc
+++ 19/solve.cc
@@ -1,20 +1,19 @@
 #include "aoc.h"
 namespace {
+struct TrieNode;
+struct NodePtr : std::unique_ptr<TrieNode> { inline NodePtr(); };
 struct TrieNode {
-   std::unordered_map<char, std::unique_ptr<TrieNode>> arcs;
+   std::unordered_map<char, NodePtr> arcs;
    bool istok{false};
 };
+NodePtr::NodePtr() : std::unique_ptr<TrieNode>(new TrieNode) {}
 
 struct Trie {
    TrieNode root;
-   void insert(std::string_view sv) {
+   void insert(std::string_view sv) noexcept {
       TrieNode *cur = &root;
-      for (char c : sv) {
-         auto &arc = cur->arcs[c];
-         if (!arc) 
-            arc = std::make_unique<TrieNode>();
-         cur = arc.get();
-      }
+      for (char c : sv)
+         cur = cur->arcs[c].get();
       cur->istok = true;
    }
 };
@@ -25,20 +24,18 @@ struct Input {
    Input(std::istream &is) noexcept {
       std::string line;
       getline(is, line);
-      std::string_view sv = line;
-      while (sv != "")
+      for (std::string_view sv = line; sv != ""; )
          flags.insert(aoc::parsetoken<std::string_view>(sv, ", "));
       std::getline(is, line);
-      assert(line == "");
       while (std::getline(is, line))
          patterns.push_back(line);
    }
 };
 
-unsigned long long solve_pattern(const Trie &flags, std::string_view remaining_pattern) {
+unsigned long solve_pattern(const Trie &flags, std::string_view remaining_pattern) {
    using Paths = std::unordered_map<const TrieNode *, unsigned long long>;
    Paths in;
-   in[&flags.root]++;
+   in[&flags.root] = 1;
    for (char c : remaining_pattern) {
       Paths out;
       for (const auto [ node, count ] : in ) {
@@ -52,11 +49,8 @@ unsigned long long solve_pattern(const Trie &flags, std::string_view remaining_p
       }
       in = std::move(out);
    }
-   unsigned long long count = 0;
-   for (auto &[k,v] : in)
-      if (k->istok)
-         count += v;
-   return count;
+   return std::accumulate(in.begin(), in.end(), 0ULL, [](unsigned long acc, auto item) {
+         return item.first->istok ? acc + item.second : acc; });
 }
 
 void part1(std::istream &in, std::ostream &out) {
