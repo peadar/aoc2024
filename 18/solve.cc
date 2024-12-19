0223c3ab1eebf4681104c359a7e57f3705d99907
Peter Edwards <peadar@arista.com>
2024-12-19 11:16:55 +0000

    day 18, using BFS

--- 18/solve.cc
+++ 18/solve.cc
@@ -1,99 +1,88 @@
 #include "aoc.h"
-
 namespace {
-enum Direction { NORTH, EAST, SOUTH, WEST };
 
 using Scalar = unsigned;
-using Int = unsigned short;
-constexpr Int MAX = std::numeric_limits<Int>::max();
+using Cost = unsigned;
+constexpr Cost MAXCOST = std::numeric_limits<Cost>::max();
+static constexpr Scalar MAXROWS = 71;
+static constexpr Scalar MAXCOLS = 71;
 
-struct Point {
-   Scalar row{}, col{};
-   Point operator+ (const Point &rhs) const noexcept { return { Scalar(row + rhs.row), Scalar(col + rhs.col) }; }
-   auto operator <=> (const Point &rhs) const = default;
-};
+#if 0
+using Point = std::complex<Scalar>;
+constexpr Scalar row(const Point &p) noexcept { return p.imag(); }
+constexpr Scalar col(const Point &p) noexcept { return p.real(); }
+constexpr unsigned key(Point p) noexcept { return p.imag() * MAXCOLS + p.real(); }
+constexpr Point make_point(Scalar row, Scalar col) noexcept { return { col, row }; }
+#else
+using Point = unsigned;
+constexpr Scalar row(Point p) noexcept { return p / MAXCOLS; }
+constexpr Scalar col(Point p) noexcept { return p % MAXCOLS; }
+constexpr unsigned key(Point p) noexcept { return p; }
+constexpr Point make_point(Scalar row, Scalar col) noexcept { return row * MAXCOLS + col; }
+#endif
 
-struct Cell {
-   unsigned short wall:1{};
-   unsigned short epoch:15{};
-   Int cost;
-};
-
-constexpr Point velocity(Direction d) noexcept {
-   switch (d) {
-      case NORTH: return {Scalar(-1), 0};
-      case SOUTH: return {1, 0};
-      case EAST: return {0, 1};
-      case WEST: return {0, Scalar(-1)};
-      default: __builtin_unreachable();
-   }
-}
-
-struct Node {
-   unsigned long cost { MAX };
-   Point pos{};
-   auto operator <=> (const Node &rhs) const = default;
-};
-
-static constexpr Scalar ROWS = 71;
-static constexpr Scalar COLS = 71;
+struct Cell { Cost cost{MAXCOST}; };
+using Map = std::array<Cell, MAXCOLS * MAXROWS>;
+Cell & at(Map &map, Point p) { return map[key(p)]; }
 
 struct MemorySpace {
-   std::vector<Cell> map;
-   unsigned age{};
-   Point s{0,0}, e{ROWS-1, COLS-1};
-
-   Cell & at(Point p) { return map[p.row * COLS + p.col]; }
+   Map weights;
    std::vector<Point> walls;
-   const Cell & at(Point p) const { return map[p.row * COLS + p.col]; }
 
-   Int dijkstra(unsigned maxage) {
-      for (auto &cell : map)
-         cell.cost = MAX;
-      std::set<Node> Q;
-      Q.insert({.cost=0, .pos = {0,0}});
-      while (Q.begin()->pos != e) {
-         auto head = Q.begin();
-         auto item = *head;
-         Q.erase(head);
-         auto try_add = [&](Point p, Int c) {
-            if (p.row >= ROWS || p.col >= COLS)
-               return;
-            auto &cell = at(p);
-            if ((cell.wall && cell.epoch < maxage) || cell.cost < c )
+   static constexpr Point s { make_point(0,0) };
+   static constexpr Point e { make_point( MAXROWS-1, MAXCOLS-1) };
+
+   Cost bfs(Cost maxage) {
+      Map map { weights };
+      std::deque<Point> Q;
+      at(map, make_point(0,0)).cost = 0;
+      Q.push_back(make_point(0,0));
+      while (*Q.begin() != e) {
+         auto from = Q.front();
+         Q.pop_front();
+         auto try_add = [&](const Point &to, Cost c) {
+            auto &cell = at(map, to);
+            if (cell.cost < maxage)
                return;
             cell.cost = c;
-            Q.insert({.cost=c, .pos=p });
+            Q.push_back(to);
          };
-         try_add(item.pos + velocity(NORTH), item.cost + 1);
-         try_add(item.pos + velocity(SOUTH), item.cost + 1);
-         try_add(item.pos + velocity(EAST), item.cost + 1);
-         try_add(item.pos + velocity(WEST), item.cost + 1);
+         const Cell &existing = at(map, from);
+         if (row(from) != 0)
+            try_add(make_point(row(from) - 1, col(from)), existing.cost + 1);
+         if (row(from) != MAXROWS - 1)
+            try_add(make_point(row(from) + 1, col(from)), existing.cost + 1);
+         if (col(from) != 0)
+            try_add(make_point(row(from), col(from) - 1), existing.cost + 1);
+         if (col(from) != MAXCOLS - 1)
+            try_add(make_point(row(from), col(from) + 1), existing.cost + 1);
          if (Q.empty())
-            return MAX;
+            return MAXCOST;
       }
-      return Q.begin()->cost;
+      return at(map, *Q.begin()).cost;
    }
 
-   MemorySpace(std::istream &is) noexcept : map ( ROWS * COLS ) {
+   MemorySpace(std::istream &is) noexcept {
+      walls.reserve(4000);
       for (std::string line; getline(is, line); ) {
          std::string_view v = line;
          Scalar col = aoc::parsetoken<int>(v, ",");
          Scalar row = aoc::parsetoken<int>(v, "\n");
-         walls.emplace_back(row, col);
-         auto &cell = at({row, col});
-         cell.wall = true;
-         cell.epoch = age++;
+         auto p = make_point( row, col );
+         auto &cell = at(weights, p);
+         cell.cost = walls.size();
+         walls.emplace_back(p);
       }
    }
 };
-aoc::Case case1{"part1", [](std::istream &is, std::ostream &os) { os << MemorySpace(is).dijkstra(1024); } };
+
+aoc::Case case1{"part1", [](std::istream &is, std::ostream &os) { os << MemorySpace(is).bfs(1024); } };
 
 struct Part2 : MemorySpace {
    Part2( std::istream &is ) : MemorySpace(is) {}
    void part2(std::ostream &os) {
-      auto res = std::ranges::partition_point( std::ranges::iota_view(unsigned(0), age) , [this] ( unsigned value ) { auto v = dijkstra(value); return v != MAX; });
-      os << walls[*res - 1].col << "," << walls[*res - 1].row << "\n";
+      auto res = std::ranges::partition_point( std::ranges::iota_view(Cost(0), Cost(walls.size())) , [this] ( Cost v ) { return bfs(v) != MAXCOST; });
+      os << col(walls[*res - 1]) << "," << row(walls[*res - 1]) << "\n";
    }
 };
 aoc::Case case2{"part2", [](std::istream &is, std::ostream &os) { Part2(is).part2(os); } };
